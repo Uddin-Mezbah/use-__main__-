27c38731a92bf2ba0c6a5119ee597762e557a964
Mezbah uddin(davide) <75944104+davide666777@users.noreply.github.com>
2021-02-22 00:23:14 +0800

    Add files via upload

---
+++ file4.py
@@ -0,0 +1,13 @@
+#import sklearn
+
+def printdav(string):
+    return f"please god give me this (string)"
+
+def add(num1,num2):
+    return num1 + num2 + 10
+
+print("aand the name is", __name__)
+if __name__ == '__main__':
+    print(printdav("davide1"))
+    o = add(5,6)
+    print(o)
